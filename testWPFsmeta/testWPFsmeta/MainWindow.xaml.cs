56ad77b936d4fafe2d85ca1008a4c0bacf0cf538
tix836 <43826310+tix836@users.noreply.github.com>
2020-07-10 01:29:57 +0700

    Add files via upload
    Примерная версия гуи и кривой класс для добавления данных

---
+++ testWPFsmeta/testWPFsmeta/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using testWPFsmeta.Models;
+
+namespace testWPFsmeta
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<SmetaModel> _smetaData;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            _smetaData = new BindingList<SmetaModel>()
+            {
+                new SmetaModel(){ Name = "Покраска", Count = 4, Cost = 100 },
+                new SmetaModel(){ Name = "Шпаклевка", Count = 6, Cost = 120 }
+            };
+            dgSmeta.ItemsSource = _smetaData;
+        }
+    }
+    
+}
---
+++ testWPFsmeta/testWPFsmeta/Models/SmetaModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testWPFsmeta.Models
+{
+    class SmetaModel
+    {
+        public string Name { get; set; }
+        private int _Count;
+        private int _Cost;
+        
+        public int Count
+        {
+            get { return _Count; }
+            set { _Count = value; }
+        }
+        
+        public int Cost
+        {
+            get { return _Cost; }
+            set { _Cost = value; }
+        }
+
+
+    }
+}
