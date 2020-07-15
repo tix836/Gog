10204c5c7a1625f2f2c15a5dd9454f33eb56b20d
tix836 <43826310+tix836@users.noreply.github.com>
2020-07-16 01:55:01 +0700

    Категория в коде
    Отрефакторить и обернуть в кнопку

--- testWPFsmeta/testWPFsmeta/MainWindow.xaml.cs
+++ testWPFsmeta/testWPFsmeta/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -22,21 +23,79 @@ namespace testWPFsmeta
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BindingList<SmetaModel> _smetaData;
+        public BindingList<SmetaModel> _smetaDataList;
+        //private BindingList<SmetaModel> _smetaDataList;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        public class Info
+        {
+            public string Name { get; set; }
+            public string Age { get; set; }
+        }
         private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
         {
-            _smetaData = new BindingList<SmetaModel>()
+            _smetaDataList = new BindingList<SmetaModel>() // по сути пустой лист
             {
-                new SmetaModel(){ Name = "Покраска", Count = 4, Cost = 100 },
-                new SmetaModel(){ Name = "Шпаклевка", Count = 6, Cost = 120 }
+                new SmetaModel(){},
             };
-            dgSmeta.ItemsSource = _smetaData;
+            dgSmeta.ItemsSource = _smetaDataList;
+            dgSmet.ItemsSource = _smetaDataList;
+
+         // ЗАВЕРНУТЬ В КНОПКУ НАЧАЛО
+
+            // создание итема в дереве
+            TreeViewItem item = new TreeViewItem();
+            item.Header = "asds";
+            item.Name = "item_name";
+            dgTree.Items.Add(item);
+
+            // создание датагрида
+            var nameBinding = new Binding("Name")
+            {
+                Mode = BindingMode.Default,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+            var ageBinding = new Binding("Age")
+            {
+                Mode = BindingMode.Default,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+            var dataGrid = new DataGrid();
+            dataGrid.AutoGenerateColumns = false;
+            dataGrid.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Name",
+                Binding = nameBinding
+            });
+            dataGrid.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Age",
+                Binding = ageBinding
+            });
+            dataGrid.ItemsSource = new ObservableCollection<Info>
+            {
+                new Info
+                {
+                    Name = "Name 1",
+                    Age = "100"
+                }
+            };
+
+            item.Items.Add(dataGrid);
+        // ЗАВЕРНУТЬ В КНОПКУ КОНЕЦ
+
         }
+
+
+
+
+
+
+
     }
-    
 }
+
+  
+
